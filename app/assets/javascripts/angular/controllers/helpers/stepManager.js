bd56f0f570b222dfc3ab53564ec00d07824279ef
kaaterskil <blair@kaaterskil.com>
2014-08-25 17:41:54 -0400

    Add spin/grow/shrink/link box features

--- app/assets/javascripts/angular/controllers/helpers/stepManager.js
+++ app/assets/javascripts/angular/controllers/helpers/stepManager.js
@@ -16,13 +16,50 @@
       var letters = '0123456789ABCDEF'.split('');
       var color = '#';
       for (var i = 0; i < 6; i++ ) {
-          color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
       }
       return color;
     }
 
     function radiansToDegrees(radians){
       return Math.floor((radians + Math.PI) * 180 / Math.PI);
+    }
+
+    function showBox(stepTitle, degrees){
+      var stepId = '#item' + _store[stepTitle].id();
+
+      var continueBtn = document.createElement('button');
+      $(continueBtn).addClass('step-continue-btn');
+      $(continueBtn).text(_store[stepTitle].continueBtnText());
+      $(continueBtn).on('click', function(){
+        processBox(_store[stepTitle]);
+      });
+
+      var formContainer = document.createElement('div');
+      $(formContainer).addClass('step-form-container');
+      $(formContainer).append($(continueBtn));
+      $(stepId + ' .step-box').append($(formContainer));
+
+      $(stepId)
+      .velocity({ zIndex: 1000, rotateZ: -degrees }, { duration: 0 })
+      .velocity({ scale: 2 }, { duration: 500 });
+    }
+
+    function hideBox(step, zIndexValue){
+      var stepId = '#item' + step.id();
+      $(stepId)
+      .velocity({ zIndex: zIndexValue }, { duration: 0 })
+      .velocity({ scale: 1 }, { duration: 500 });
+      $(stepId).remove('.step-continue-btn');
+    }
+
+    function processBox(step){
+      var nextStep;
+      if(step.responses.length <= 1) {
+        nextStep = step.responses()[0].getNextStep();
+      }
+      hideBox(step, $('#item' + nextStep.id()).css('z-index'));
+      spin(nextStep.title());
     }
 
     //---------- Public functions ----------//
@@ -32,19 +69,17 @@
       width = $this.width(),
       height = $this.height(),
       radius = 700,
+      outerRadius = 862,
       radians = Math.PI,
       textRadians = radians,
       keys = Object.getOwnPropertyNames(_store),
       numSteps = keys.length,
       radIncrement = (Math.PI * 2) / numSteps,
-      origin = {x: width / 2, y: 862},
+      origin = {x: width / 2, y: outerRadius},
       color = randomColor(),
       step, key, category, stepContainer, stepBox, color, newX, newY, rotation;
 
-      $('#wheel-container').css({
-        'top': 300,
-        'left': (origin.x - 862) + 'px'
-      });
+      $('#wheel-container').css({ 'left': (origin.x - 862) + 'px' });
 
       // iterate through the store
       initializeIndex();
@@ -53,8 +88,8 @@
         step = _index[j];
 
         // Compute top and left coordinates
-        newX = Math.floor(origin.x + (Math.sin(radians) * radius) - 100);
-        newY = Math.floor(origin.y + (Math.cos(radians) * radius) - 162);
+        newX = Math.floor(outerRadius - 100 + (Math.sin(radians) * radius));
+        newY = Math.floor(outerRadius - 162 + (Math.cos(radians) * radius));
 
         // Compute other attributes
         if(category !== step.category()) {
@@ -70,9 +105,10 @@
           'id': step.id(),
           'title': step.title()
         });
+        $(stepBox).append('<div class="step-category">' + step.category() + '</div>');
         $(stepBox).append('<div class="step-title">' + step.title() + '</div>');
         $(stepBox).append('<div class="step-text">' + step.text() + '</div>');
-        // $(stepBox).append('<div class="step-id">' + (j + 1) + '</div>');
+        $(stepBox).append()
         $(stepBox).css({'background': color});
 
         // Create and position the box container
@@ -91,7 +127,6 @@
         $('#wheel').append($(stepContainer));
 
         // Set the step rotation
-        // _store[key].rotation(radians);
         _index[j].rotation(radians);
 
         // Increment the rotation for the nexxt iteration
@@ -102,14 +137,18 @@
 
     function spin(stepTitle){
       // Set the new wheel rotation
-      radians = parseFloat(_store[stepTitle].rotation()) || 0,
+      var radians = parseFloat(_store[stepTitle].rotation()) || Math.PI,
       degrees = radiansToDegrees(radians);
 
-      // Now spin it 3x
-      $('#item' + _store[stepTitle].id()).css('z-index', 1000);
-      $('#wheel').velocity({transformOriginX: '862px', transformOriginY: '862px'});
-      $('#wheel').velocity({ rotateZ: (1080 + degrees) }, { duration: 1500 })
-      .velocity({scaleX: 2, scaleY: 2 }, { duration: 2000 });
+      // Now animate it, chaining other animations to the complete property
+      $('#wheel').velocity({
+        rotateZ: 1080 + degrees
+      }, {
+        duration: 1500,
+        complete: function(){
+          showBox(stepTitle, degrees);
+        }
+      });
     }
 
     function get(title){
